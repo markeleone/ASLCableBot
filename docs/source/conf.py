43fab9b9fa7465e5c937e70e43df39b133ee4d6d
markeleone <85571768+markeleone@users.noreply.github.com>
2023-05-25 00:03:43 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CableBot'
+copyright = '2023, Mark Leone'
+author = 'Mark Leone'
 
 release = '0.1'
 version = '0.1.0'
